aef40ebc82f354ffff8014d41676dd1c78e46ffb
Zypherone <danutuckersaunders@email.com>
2020-04-22 21:19:03 +1000

    Added prompt test

--- docs/readme.md/index.js
+++ docs/readme.md/index.js
@@ -1,1 +1,23 @@
-consts
+const fs             = require('fs');
+const util           = require('util');
+const inquirer       = require('inquirer');
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const runPrompt = () => {
+
+  return true;
+
+}
+
+(async () =>  {
+
+  try {
+    const result = await runPrompt();
+
+    console.log(result);
+  }
+  catch (error) {
+    console.log(error);
+  }
+  
+})();
